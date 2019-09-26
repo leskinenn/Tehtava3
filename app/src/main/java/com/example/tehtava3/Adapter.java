c06a5b42ee01ffc5c4d5f16124e83d26a3756c7f
leskinenn <Pekka.Leskinen2@edu.savonia.fi>
2019-09-27 01:06:18 +0300

    Ei toimi ei

---
+++ app/src/main/java/com/example/tehtava3/Adapter.java
@@ -0,0 +1,45 @@
+package com.example.tehtava3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Adapter extends ArrayAdapter<Date> {
+    public Button lisaa;
+    private Context konteksti;
+    private ArrayList<Date> dates = new ArrayList<>();
+    private ArrayList<String> vitunvittu = new ArrayList<>();
+
+    public Adapter(@NonNull Context context, ArrayList<Date> pvm) {
+        super(context, 0, pvm);
+        //this.konteksti = context;
+        //this.dates = pvm;
+    }
+    @Override
+
+    public View getView(final int position, View convertView, ViewGroup parent){
+        TextView teksti = (TextView) convertView.findViewById(R.id.textView1);
+        teksti.setText(dates.get(0).toString());
+
+        LayoutInflater inflater = (LayoutInflater)konteksti.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView=inflater.inflate(R.layout.listview_row,null,true);
+        final TextView nameTextField = (TextView) rowView.findViewById(R.id.textView1);
+        nameTextField.setText(dates.get(position).toString());
+        return convertView;
+    }
+
+}
